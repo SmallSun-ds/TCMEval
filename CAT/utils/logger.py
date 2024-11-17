73fbf0ee4168efc441f0a9387ade89ca4df4f50b
sdslrn <2546914721@qq.com>
2024-11-17 17:06:41 +0800

    CAT

---
+++ CAT/utils/logger.py
@@ -0,0 +1,11 @@
+import logging
+import sys
+
+def setuplogger():
+    root = logging.getLogger()
+    root.setLevel(logging.INFO)
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setLevel(logging.INFO)
+    formatter = logging.Formatter("[%(levelname)s %(asctime)s] %(message)s")
+    handler.setFormatter(formatter)
+    root.addHandler(handler)
